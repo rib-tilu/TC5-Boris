d438a5bb713add69bc3d46f11272a6cb67a540e1
rib.tilu <rib.tilu@gmail.com>
2022-11-18 10:27:12 +0100

    correction moment mag

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from classes import *
 import numpy as np
 from graphes import *
 from champs import B_uniforme as f_B
-from champs import E_onde_plane as f_E
+from champs import E_uniforme as f_E
 
 
 ### Constantes numériques ###
 
 
-Nt = 1000 ## nombre de pas temporels
+Nt = 100 ## nombre de pas temporels
 
 Nc = 20 ## nombre de pas par orbite
 
@@ -41,6 +41,8 @@ V = Vector(1000000, 1000000, 1000000)
 
 traj_x, traj_y, traj_z = [], [], []
 
+V_x, V_y, V_z = [], [], []
+
 Ec, Mu, Rlarmor, temps = [], [], [], []
 
 
@@ -48,9 +50,9 @@ Ec, Mu, Rlarmor, temps = [], [], [], []
 
 B = Vector(0, 0, m*omega/abs(q))
 
-RE = Vector(100000,-100000,0)
+RE = Vector(100000, 0, 100000)
 
-IE = Vector(0,-1000,1000)
+IE = Vector(0,0,0)
 
 
 
@@ -71,7 +73,7 @@ for k in range(Nt):
     
     B = f_B(B, Xinter, k*dt)
     
-    E = f_E(RE, IE, omega, Xinter, k*dt)
+    E = f_E(RE, IE, Xinter, k*dt)
     
     
     phase = step(Xinter, V, B, E, q, m, dt) # tableau pour stocker [X,V]
@@ -86,12 +88,16 @@ for k in range(Nt):
     traj_y.append(X.y*100)
     traj_z.append(X.z*100)
     
+    V_x.append(V.x)
+    V_y.append(V.y)
+    V_z.append(V.z)
+    
     
     Ec.append( m*produit_scalaire(V, V)/(2*abs(q)) )
     
-    Rlarmor.append( m*( (produit_scalaire( produit_vectoriel( produit_vectoriel(V,B) , B ) , produit_vectoriel( produit_vectoriel(V,B) , B ) ))**0.5 )/(abs(q)*(B.norm()**3)) )
+    Rlarmor.append( 100*m*( (produit_scalaire( produit_vectoriel( produit_vectoriel(V,B) , B ) , produit_vectoriel( produit_vectoriel(V,B) , B ) ))**0.5 )/(abs(q)*(B.norm()**3)) )
 
-    Mu.append( m*produit_scalaire( produit_vectoriel( produit_vectoriel(V,B) , B ) , produit_vectoriel( produit_vectoriel(V,B) , B ) )/(2*(B.norm()**3)) )
+    Mu.append( m*produit_scalaire( produit_vectoriel( produit_vectoriel(V,B) , B ) , produit_vectoriel( produit_vectoriel(V,B) , B ) )/(2*(B.norm()**5)) )
     
     temps.append(dt*k*10**12)
 
@@ -106,6 +112,13 @@ plot_1d_trajs(traj_x, traj_y, "Trajectoire dans le plan (x,y)", "x [cm]", "y [cm
 plot_1d_trajs(traj_z, traj_y, "Trajectoire dans le plan (y,z)", "z [cm]", "y [cm]")
 
 plot_3d(traj_x, traj_y, traj_z, "Trajectoire 3D", "X (cm)", "Y (cm)", "Z (cm)")
+
+
+# plot_1d_trajs(temps, V_x, "Vx", "t [ps]", r"Vx [m.s$^{-1}$]")
+
+# plot_1d_trajs(temps, V_y, "Vy", "t [ps]", r"Vy [m.s$^{-1}$]")
+
+# plot_1d_trajs(temps, V_z, "Vz", "t [ps]", r"Vz [m.s$^{-1}$]")
 
 
 plot_1d_graphs(temps, Ec, "Energie cinétique", "t [ps]", "Ec [eV]")
